7d573b920d8c20238209c0a92c93261a123e9956
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-25 09:49:12 +0530

    Undo previous chnages in index.js file

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL!.split(','),
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
